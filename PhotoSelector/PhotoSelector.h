e9d675956b6bbd509573fde267e18eebdc8068d7
ryan johnson <ryan@mobiledatasolutions.net>
2013-07-05 16:47:21 -0500

    Initial Commit

---
+++ PhotoSelector/PhotoSelector.h
@@ -0,0 +1,13 @@
+//
+//  PhotoSelector.h
+//  PhotoSelector
+//
+//  Created by Ryan Johnson on 7/5/13.
+//  Copyright (c) 2013 MDS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotoSelector : NSObject
+
+@end
---
+++ PhotoSelector/PhotoSelector.m
@@ -0,0 +1,13 @@
+//
+//  PhotoSelector.m
+//  PhotoSelector
+//
+//  Created by Ryan Johnson on 7/5/13.
+//  Copyright (c) 2013 MDS. All rights reserved.
+//
+
+#import "PhotoSelector.h"
+
+@implementation PhotoSelector
+
+@end
